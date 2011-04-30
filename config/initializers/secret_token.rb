81b446d015b6a8946d0119d722933800b2f22be5
Paul Piscuc <cake.eater.mm@gmail.com>
2011-04-30 19:51:44 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doctify::Application.config.secret_token = 'f9a59ea898db649b46fd35b8015eb53a12cf955ad79055933a07f0b52aaebcd27c33e1d6476950d02159fd2a2f9cbede9102685fa6c4d3cb92903c86686f2467'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Doctify::Application.config.session_store :cookie_store, :key => '_doctify_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Doctify::Application.config.session_store :active_record_store
